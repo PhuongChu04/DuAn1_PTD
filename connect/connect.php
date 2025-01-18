a626096c7c8cb40e143811dfd06908b29b2a3438
NgoThanh11 <thanhntph49043@gmail.com>
2025-01-18 17:13:15 +0700

    addCategory

--- connect/connect.php
+++ connect/connect.php
@@ -1,9 +1,21 @@
 <?php
 
     //kết nối cơ sở dữ liệu
-
-    // oop trong lập trình
-    // - lập trình hướng đối tượng dựa trên lớp và đối tượng 
-    // ==> class object
+    class connect{
+        public function connect(){
+            $serverName = 'localhost';
+            $userName = 'root';
+            $password = '';
+            $myDB = 'duan1_ptd';
+            try {
+                $conn = new PDO("mysql:host=$serverName;dbname=$myDB", $userName, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $conn;
+            } catch (\Throwable $th) {
+                echo 'Kết nối thất bại'. $th->getMessage();
+                return null;
+            }
+        }
+    }
 
 ?>
