3779d6cf107bf653b67709e87ddf5827e9069d78
dungvtph48187 <dungvtph48187@fpt.edu.vn>
2025-02-18 09:13:07 +0700

    Add files via upload

--- connect/connect.php
+++ connect/connect.php
@@ -2,20 +2,24 @@
 
     //kết nối cơ sở dữ liệu
     class connect{
+        private $conn;
         public function connect(){
+            if($this->conn === null){
             $serverName = 'localhost';
             $userName = 'root';
             $password = '';
             $myDB = 'duan1_ptd';
             try {
-                $conn = new PDO("mysql:host=$serverName;dbname=$myDB", $userName, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $conn;
+                $this->conn = new PDO("mysql:host=$serverName;dbname=$myDB", $userName, $password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
             } catch (\Throwable $th) {
                 echo 'Kết nối thất bại'. $th->getMessage();
                 return null;
             }
         }
+        return $this->conn;
+    }
     }
 
 ?>
